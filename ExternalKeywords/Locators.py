104b518da9d1d591b898f29b203ac98fa54c0961
RamZsG <91100836+RamZsG@users.noreply.github.com>
2021-12-14 19:44:26 -0500

    Update Locators.py

--- ExternalKeywords/Locators.py
+++ ExternalKeywords/Locators.py
@@ -2,11 +2,11 @@ import json
 import jsonpath 
 
 
-def read_Locator_From_Json(locator_Name):
+def read_Locator_From_Json(locatorName):
     f=open('C:/Users/RamSchool/Documents/RobotFrameworkVSCode/JSonFile/Elements.json')
     response=json.loads(f.read())
-    value=jsonpath.jsonpath(response, locator_Name)
+    value=jsonpath.jsonpath(response, locatorName)
     return  value[0]
 
-#l=read_Locator_From_Json('login_btn_id')
-#print(l)
+l=read_Locator_From_Json('login_btn_id')
+print(l)
