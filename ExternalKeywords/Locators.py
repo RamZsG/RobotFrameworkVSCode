dd8cd8c55559f25495f7ad031d66cfe71b26147a
RamZsG <91100836+RamZsG@users.noreply.github.com>
2021-12-16 16:55:35 -0500

    cleanup

--- ExternalKeywords/Locators.py
+++ ExternalKeywords/Locators.py
@@ -7,11 +7,9 @@ def read_Locator_From_Json(locatorName):
     f=open('C:/Users/RamSchool/Documents/RobotFrameworkVSCode/JSonFile/Elements.json')
     response=json.loads(f.read())
     value=jsonpath.jsonpath(response, locatorName)
-    str_value=json.dumps(value[0])
-
     # value= [match.value for match in parse (locatorName).find(response)]
+    str_value=json.dumps(value[0])
     # print(value)
-    # str_value=json.dumps(value)
     return  str_value.strip('"')
 
 # l=read_Locator_From_Json('Login.username_TextInput_id')
